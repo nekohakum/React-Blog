c92db8e47e4b3c46c1462e455d00b3bfd275e2e6
qw1216 <qiuwei1216@foxmail.com>
2023-11-15 14:59:22 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import EditPost from "./components/EditPost";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+import api from "./api/posts";
+import useAxiosFetch from "./hooks/useAxiosFetch";
+import useWindowSize from "./hooks/useWindowSize";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+      const [posts, setPosts] = useState([]);
+      const [search, setSearch] = useState("");
+      const [searchResults, setSearchResults] = useState([]);
+      const [postTitle, setPostTitle] = useState("");
+      const [postBody, setPostBody] = useState("");
+      const [editTitle, setEditTitle] = useState("");
+      const [editBody, setEditBody] = useState("");
+      const history = useHistory();
+      const { width } = useWindowSize();
+
+      const { data, fetchError, isLoading } = useAxiosFetch(
+           "http://localhost:3500/posts"
+      );
+
+      useEffect(() => {
+           setPosts(data);
+      }, [data]);
+
+      useEffect(() => {
+           const filteredResults = posts.filter(
+                (post) =>
+                     post.body.toLowerCase().includes(search.toLowerCase()) ||
+                     post.title.toLowerCase().includes(search.toLowerCase())
+           );
+
+           setSearchResults(filteredResults.reverse());
+      }, [posts, search]);
+
+      const handleSubmit = async (e) => {
+           e.preventDefault();
+           const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+           const datetime = format(new Date(), "MMMM dd, yyyy pp");
+           const newPost = { id, title: postTitle, datetime, body: postBody };
+           try {
+                const response = await api.post("/posts", newPost);
+                const allPosts = [...posts, response.data];
+                setPosts(allPosts);
+                setPostTitle("");
+                setPostBody("");
+                history.push("/");
+           } catch (err) {
+                console.log(`Error: ${err.message}`);
+           }
+      };
+
+      const handleEdit = async (id) => {
+           const datetime = format(new Date(), "MMMM dd, yyyy pp");
+           const updatedPost = {
+                id,
+                title: editTitle,
+                datetime,
+                body: editBody,
+           };
+           try {
+                const response = await api.put(`/posts/${id}`, updatedPost);
+                setPosts(
+                     posts.map((post) =>
+                          post.id === id ? { ...response.data } : post
+                     )
+                );
+                setEditTitle("");
+                setEditBody("");
+                history.push("/");
+           } catch (err) {
+                console.log(`Error: ${err.message}`);
+           }
+      };
+
+      const handleDelete = async (id) => {
+           try {
+                await api.delete(`/posts/${id}`);
+                const postsList = posts.filter((post) => post.id !== id);
+                setPosts(postsList);
+                history.push("/");
+           } catch (err) {
+                console.log(`Error: ${err.message}`);
+           }
+      };
+
+      return (
+           <div className="App">
+                <Header title="React JS Blog" width={width} />
+                <Nav search={search} setSearch={setSearch} />
+                <Switch>
+                     <Route exact path="/">
+                          <Home
+                               posts={searchResults}
+                               fetchError={fetchError}
+                               isLoading={isLoading}
+                          />
+                     </Route>
+                     <Route exact path="/post">
+                          <NewPost
+                               handleSubmit={handleSubmit}
+                               postTitle={postTitle}
+                               setPostTitle={setPostTitle}
+                               postBody={postBody}
+                               setPostBody={setPostBody}
+                          />
+                     </Route>
+                     <Route path="/edit/:id">
+                          <EditPost
+                               posts={posts}
+                               handleEdit={handleEdit}
+                               editTitle={editTitle}
+                               setEditTitle={setEditTitle}
+                               editBody={editBody}
+                               setEditBody={setEditBody}
+                          />
+                     </Route>
+                     <Route path="/post/:id">
+                          <PostPage posts={posts} handleDelete={handleDelete} />
+                     </Route>
+                     <Route path="/about" component={About} />
+                     <Route path="*" component={Missing} />
+                </Switch>
+                <Footer />
+           </div>
+      );
+};
 
 export default App;
